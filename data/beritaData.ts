b06007c6b7af74b123009954f7d5500494b418a8
NabilApipp <nabilapipp@gmail.com>
2026-05-03 13:04:45 +0700

    chore (beritaData): create data for berita and pengumuman

---
+++ data/beritaData.ts
@@ -0,0 +1,52 @@
+export interface BeritaItem {
+  id: number;
+  title: string;
+  date: string;
+  excerpt: string;
+  image: string;
+}
+
+export const berita: BeritaItem[] = [
+  {
+    id: 1,
+    title: "Juara 1 Kompetisi Robotic Tingkat Provinsi",
+    date: "2023-10-27",
+    excerpt: "Tim robotic SMK Wikrama berhasil meraih juara 1 dalam Kompetisi Robotic Tingkat Provinsi yang diselenggarakan di Gedung Sate.",
+    image: "/images/news/robot.jpg",
+  },
+  {
+    id: 2,
+    title: "Web Programming Competition 2023",
+    date: "2023-10-20",
+    excerpt: "Kompetisi web programming diikuti oleh siswa SMK se-Bandung Raya. SMK Wikrama berhasil meraih juara 2.",
+    image: "/images/news/web.jpg",
+  },
+  {
+    id: 3,
+    title: "Juara 1 Kompetisi Robotic Tingkat Provinsi",
+    date: "2023-10-27",
+    excerpt: "Tim robotic SMK Wikrama berhasil meraih juara 1 dalam Kompetisi Robotic Tingkat Provinsi yang diselenggarakan di Gedung Sate.",
+    image: "/images/news/robot.jpg",
+  },
+  {
+    id: 4,
+    title: "Web Programming Competition 2023",
+    date: "2023-10-20",
+    excerpt: "Kompetisi web programming diikuti oleh siswa SMK se-Bandung Raya. SMK Wikrama berhasil meraih juara 2.",
+    image: "/images/news/web.jpg",
+  },
+  {
+    id: 5,
+    title: "Juara 1 Kompetisi Robotic Tingkat Provinsi",
+    date: "2023-10-27",
+    excerpt: "Tim robotic SMK Wikrama berhasil meraih juara 1 dalam Kompetisi Robotic Tingkat Provinsi yang diselenggarakan di Gedung Sate.",
+    image: "/images/news/robot.jpg",
+  },
+  {
+    id: 6,
+    title: "Web Programming Competition 2023",
+    date: "2023-10-20",
+    excerpt: "Kompetisi web programming diikuti oleh siswa SMK se-Bandung Raya. SMK Wikrama berhasil meraih juara 2.",
+    image: "/images/news/web.jpg",
+  },
+];
