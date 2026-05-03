1fecdf455f2a0bc67b1531bfb97fcc1490483333
NabilApipp <nabilapipp@gmail.com>
2026-05-03 13:02:15 +0700

    chore (techKurikulum): create data techKurikulum

---
+++ data/techKurikulum.ts
@@ -0,0 +1,57 @@
+export interface TeknologiItem {
+    id: number;
+    icon: string;
+    name: string;
+    description: string;
+}
+
+export const teknologi: TeknologiItem[] = [
+  {
+    id: 1,
+    icon: "/asset/image/icon-tech/html-5-icon.png",
+    name: "HTML",
+    description: "Membangun struktur dan desain dasar web.",
+  },
+  {
+    id: 2,
+    icon: "/asset/image/icon-tech/css-icon.png",
+    name: "CSS",
+    description: "Membangun struktur dan desain dasar web.",
+  },
+  {
+    id: 3,
+    icon: "/asset/image/icon-tech/javascript-icon.png",
+    name: "JavaScript",
+    description: "Membuat website interaktif dan dinamis.",
+  },
+  {
+    id: 4,
+    icon: "/asset/image/icon-tech/mysql-icon.png",
+    name: "MySql",
+    description: "Bahasa pemrograman server-side.",
+  },
+  {
+    id: 5,
+    icon: "/asset/image/icon-tech/php-icon.png",
+    name: "PHP",
+    description: "Sistem version control untuk kolaborasi kode.",
+  },
+  {
+    id: 6,
+    icon: "/asset/image/icon-tech/java-icon.png",
+    name: "Java",
+    description: "Belajar Pemrograman Berorientasi Objek",
+  },
+  {
+    id: 7,
+    icon: "/asset/image/icon-tech/c++-icon.png",
+    name: "C++",
+    description: "Belajar Pemrograman Dasar",
+  },
+  {
+    id: 8,
+    icon: "/asset/image/icon-tech/microsoft-office-icon.png",
+    name: "Ms. Office",
+    description: "Menguasai Ms. Office",
+  },
+];
