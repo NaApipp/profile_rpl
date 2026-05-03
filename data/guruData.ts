7642b4411f2f664d3bd1b62185f0ee59ee30743c
NabilApipp <nabilapipp@gmail.com>
2026-05-03 15:56:42 +0700

    feat (guruData): create data  guru

---
+++ data/guruData.ts
@@ -0,0 +1,59 @@
+export interface GuruItem {
+  id: number;
+  nama: string;
+  jabatan: string;
+  mapel: string;
+  foto: string;
+}
+
+export const daftarGuru: GuruItem[] = [
+  {
+    id: 1,
+    nama: "AHMAD ZAENURI, S.Kom",
+    jabatan: "Ketua Jurusan RPL",
+    mapel: "-",
+    foto: "https://smkn4kendal.sch.id/media_library/employees/21807bbbe3f33a0b160749a2ce1fe666.JPG",
+  },
+  {
+    id: 2,
+    nama: "ANTIK HARIYANTI, S.Kom.",
+    jabatan: "Guru Produktif",
+    mapel: "-",
+    foto: "https://smkn4kendal.sch.id/media_library/employees/3e4fbbdcb41160f74cc338724cc5fa2a.jpeg",
+  },
+  {
+    id: 3,
+    nama: "DAYANANTO GATIE P., S.Kom., M.Si",
+    jabatan: "Guru Produktif",
+    mapel: "=",
+    foto: "https://smkn4kendal.sch.id/media_library/employees/1f7e81ed2c207295864c8ba3ec292802.JPG",
+  },
+  {
+    id: 4,
+    nama: "IDDHA ARYANTI, S.Kom., MM.",
+    jabatan: "Guru Produktif",
+    mapel: "-",
+    foto: "https://smkn4kendal.sch.id/media_library/employees/f945a22f23844f15291d2788d2ed4b01.JPG",
+  },
+  {
+    id: 5,
+    nama: "JOKO SUSILO, S.Kom., M.Si",
+    jabatan: "Guru Produktif",
+    mapel: "-",
+    foto: "https://smkn4kendal.sch.id/media_library/employees/f18f9900d8028469aed53d5f668e7c7b.JPG",
+  },
+  {
+    id: 6,
+    nama: "SAPTO RAHARJO, S.Kom",
+    jabatan: "Kepala Jurusan RPL",
+    mapel: "-",
+    foto: "/asset/svg/user.svg",
+  },
+  {
+    id: 7,
+    nama: "SITI MARIATUL KIPTIYAH, S.Pd., M.Si.",
+    jabatan: "Guru Produktif",
+    mapel: "-",
+    foto: "/asset/svg/user.svg",
+  },
+];
